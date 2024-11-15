996659819ba1c8a91f9b8f0c52d03a27708157f8
Muhammed Althaf <Mdalthaf424@gmail.com>
2024-11-15 14:40:33 +0530

    fix

--- src/AuthenticatedLayout.tsx
+++ src/AuthenticatedLayout.tsx
@@ -7,7 +7,7 @@ import { useEffect } from "react";
 import { Loader } from "lucide-react";
 
 const AuthenticatedLayout = () => {
-  const {isAuthenticated,isCheckingAuth,checkAuth} = useAuthStore()
+  const {isAuthenticated,isCheckingAuth,checkAuth,provider} = useAuthStore()
 
   useEffect(() => {
 		checkAuth();
@@ -24,16 +24,20 @@ const AuthenticatedLayout = () => {
   }
 
   return (
-    <div className="w-full h-screen flex overflow-hidden">
-      <Header />
-      <Sidebar />
-      <main className="flex-1 overflow-y-auto p-4">
-        {/* Renders the child route components */}
-        <Container>
-          <Outlet />
-        </Container>
-      </main>
-    </div>
+    <>
+    {isAuthenticated &&provider&& (
+        <div className="w-full h-screen flex overflow-hidden">
+        <Header />
+        <Sidebar />
+        <main className="flex-1 overflow-y-auto p-4">
+          {/* Renders the child route components */}
+          <Container>
+            <Outlet />
+          </Container>
+        </main>
+      </div>
+    )}
+    </>
   );
 };
 
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -13,9 +13,10 @@ import {
 import { useAuthStore } from "@/store/authStore";
 
 const Header = () => {
-  const {logout} = useAuthStore()
+  const {logout,checkAuth} = useAuthStore()
   const handleLogout = ()=> {
     logout();
+    checkAuth();
   }
   return (
     <nav className="fixed top-0 w-full h-20 z-[49] flex justify-between items-center bg-primarygrey shadow-md px-6">
